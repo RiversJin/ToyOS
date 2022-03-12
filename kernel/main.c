03d5b45291bc4d2943a0619ea5c1e9e161ea69f6
rivers <riversjin@foxmail.com>
2022-03-12 21:17:51 +0800

    添加buffer与sd系统的初始化

--- kernel/main.c
+++ kernel/main.c
@@ -4,6 +4,9 @@
 #include "interupt/interupt.h"
 #include "arch/aarch64/timer.h"
 
+#include "buffer/buf.h"
+#include "driver/mmc/sd.h"
+
 extern uint32_t read_irq_src();
 
 __attribute__((noreturn))
@@ -12,7 +15,8 @@ void main(){
         console_init();
         alloc_init();
         exception_handler_init();
-
+        binit();
+        sd_init();
         init_awake_ap_by_spintable();
         
     }
