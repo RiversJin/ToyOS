83ba62ff7dc71281d5702de23fe11cfc23061f6f
rivers <riversjin@foxmail.com>
2022-03-20 12:57:27 +0800

    完善file.h

--- kernel/file/file.h
+++ kernel/file/file.h
@@ -3,17 +3,28 @@
 
 #include "../fs/fs.h"
 #include "../sync/sleeplock.h"
-
+/**
+ * @brief 文件结构体 下面的一些字段也许可以使用union 但先实现了再考虑优化
+ * 
+ */
 struct file {
-    enum { FD_NONE, FD_PIPE, FD_INODE } type;
+    enum { FD_NONE, FD_PIPE, FD_INODE, FD_DEVICE } type;
     int ref;
     char readable;
     char writable;
+    // 这个字段给pipe用
     struct pipe* pipe;
+    // 这个字段给inode和device用
     struct inode* ip;
-    size_t off;
+
+    size_t off; // inode
+
+    int16_t major; // device
 };
 
+#define major(dev) ((dev)>>16 & 0xFFFF)
+#define minor(dev) ((dev) & 0xFFFF)
+#define mkdev(m,n) ((uint32_t)((m) << 16) | (n))
 struct inode {
     uint32_t dev; 
     uint32_t inum; // inode number
@@ -34,5 +45,8 @@ struct devsw{
     int (*read)(int, uint64_t, int);
     int (*write)(int, uint64_t, int);
 };
+
+extern struct devsw devsw[];
+#define CONSOLE 1
 
 #endif
