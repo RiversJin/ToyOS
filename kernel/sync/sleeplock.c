90114329d90d1ea68472a394e5ef86d9fa835724
rivers <riversjin@foxmail.com>
2022-03-20 17:58:01 +0800

    完善sleeplock的实现

--- kernel/sync/sleeplock.c
+++ kernel/sync/sleeplock.c
@@ -1,8 +1,10 @@
 #include "sleeplock.h"
 #include "spinlock.h"
+#include "../proc/proc.h"
 
 void init_sleep_lock(struct sleeplock* lock, char *name){
-    init_spin_lock(&lock->lk,name);
+    init_spin_lock(&lock->lk,"sleeplock");
+    lock->name = name;
     lock->locked = 0;
     lock->pid = 0;
 }
@@ -10,12 +12,11 @@ void init_sleep_lock(struct sleeplock* lock, char *name){
 void acquire_sleep_lock(struct sleeplock *lock){
     acquire_spin_lock(&lock->lk);
     while(lock->locked){
-        // sleep()
+        sleep(lock, &lock->lk);
     }
     
     lock->locked = 1;
-    // TODO: this...
-    // lock->pid = 
+    lock->pid = myproc()->pid;
     release_spin_lock(&lock->lk);
 }
 
@@ -23,17 +24,14 @@ void release_sleep_lock(struct sleeplock* lock){
     acquire_spin_lock(&lock->lk);
     lock->locked = 0;
     lock->pid = 0;
-    // TODO: this
-    // wakeup(lk)
+    wakeup(lock);
     release_spin_lock(&lock->lk);
 }
 
 bool is_current_cpu_holing_sleep_lock(struct sleeplock *lock){
-    // FIX: this
-    bool r = lock->locked;
+    bool r;
     acquire_spin_lock(&lock->lk);
-    // TODO: this
-    // r = (lock->locked&&(lock->pid == thisproc()->pid));
+    r = (lock->locked&&(lock->pid == myproc()->pid));
     release_spin_lock(&lock->lk);
     return r;
 }
